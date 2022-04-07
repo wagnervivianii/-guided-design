920e675dbf0da5f592c31ceb827b5db0d981005f
wagner viviani <devwagnervi@gmail.com>
2022-04-07 01:08:00 -0300

    Ate ediçao concluido e funcionando

--- js/form.js
+++ js/form.js
@@ -1,24 +1,51 @@
+
 //obtendo lista armazenada no storege e atribuindo a variável
-personRaw = localStorage.getItem(('list'));
+let personRaw = localStorage.getItem(('list'));
 // Testando para ver se a lista está vazia
 if (personRaw != null){
   // transformando a string recebido em objeto com o parse
-  var listOfPeople = JSON.parse(localStorage.getItem('list'))
+  var listOfPeople = JSON.parse(personRaw);
 }
+  // Caso não haja registros no localStorage, listOf people recebe []
 else{
   var listOfPeople = []
 }
 
 function registerData(e){
+  // Pprevent defalt impede que o formulário tenha ações padroẽs e execute o código até o fim
   e.preventDefault();
-  listOfPeople.push({
+  // Analisa se id é nulo, se for, continua no else para cadastro normal, se não, entra no modo EDIÇÃO
+  if(id !== null){
+    listOfPeople[id] = {
     name: e.target.elements[0].value,
     tel: e.target.elements[1].value,
-    xp: e.target.elements['xp'].value
-  })
+    xp: e.target.elements['xp'].value == 'true' ? true : false
+    }
+  }
+  // Caso o Id seja vazio, entra no modo de CADASTRO normal
+  else{
+    listOfPeople.push({
+      // Cadastrando usando o evento, utilizando o método TARGET
+      name: e.target.elements[0].value,
+      tel: e.target.elements[1].value,
+      xp: e.target.elements['xp'].value == 'true' ? true : false
+    })
+  }
+  // Armazenando o Código no localStorange, e transformando este em string com o método stringify do Json 
   localStorage.setItem('list', JSON.stringify(listOfPeople))
-  e.target.elements[0].value = ''
-  e.target.elements[1].value = ''  
+  // Selecionando o link home e clicando automaticamente nele para voltar a home page
+  document.querySelector('.home').click()
 };
 
+// Obtendo o endereço atual da página e colocando em uma variável
+var urlPrincipal = new URL(window.location.href);
+// Obtendo o index da lista através da url
+var id = urlPrincipal.searchParams.get('person')
+
+// Caso o id não seja nulo, Entra no modo Edição que preencherá os campos com os valores da lista
+if (id !== null){
+  document.querySelector('.input_nome').value = listOfPeople[id].name;
+  document.querySelector('.input_telefone').value = listOfPeople[id].tel;
+  listOfPeople[id].xp == true ? document.querySelector('#xp-yes').checked = true : document.querySelector('#xp-no').checked = true;
+}
 
--- js/index.js
+++ js/index.js
@@ -2,14 +2,12 @@
 personRaw = localStorage.getItem(('list'));
 // Testando para ver se a lista está vazia
 if (personRaw != null){
-
-  // transformando a string recebido em objeto com o parse
-  var listOfPeople = JSON.parse(localStorage.getItem('list'))
-
+// transformando a string recebido em objeto com o parse
+var listOfPeople = JSON.parse(localStorage.getItem('list'))
 }
 else{
   var listOfPeople = []
-}
+} 
 
 function drawTable() {
   //  cirando variável para acumular em uma node list, os registros em forma de pseudo array
@@ -23,20 +21,22 @@ function drawTable() {
   // Alimentando o index atravéz do innerHTML com os elemntos constantes no array de objetos listofpeople
   for(person in listOfPeople) {
       document.querySelector('table tbody').innerHTML +=`<tr>
-      <tr class="dinamic-content" style="background-color: ${(person % 2 == 0) ? '#000': 'orange'}; color: white">
+      <tr class="dinamic-content" style="background-color: ${(person % 2 == 0) ? '#836FFF': '#ffffff'}; color: #000">
         <td>${listOfPeople[person].name}</td>
         <td>${listOfPeople[person].tel}</td>
-        <td style="color: ${(listOfPeople[person].xp == true) ? "green" : "red" }; font-weight: bold; " >${listOfPeople[person].xp}<td>
+        <td style="color: ${(listOfPeople[person].xp == 'true' )  ? "#006400" : "#FF0000" }; font-weight: bold; " >${listOfPeople[person].xp}<td>
           <button onclick="deleteUser(${person})"> Excluir </button>
+          <a href="./Cadastro.html?person=${person}" ">Editar</a>
         </td>
       </tr>
     <br`
   }
 }
 
-drawTable();
 function deleteUser(p){
   listOfPeople.splice(p, 1); 
   drawTable();
   localStorage.setItem('list', JSON.stringify(listOfPeople));
 }
+
+drawTable();
